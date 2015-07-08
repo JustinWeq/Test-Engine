bc765f721d5c5dd0d4fed14e7912a77740f3ed14
JustinWeq <zoomdude111@hotmail.com>
2015-07-08 11:12:07 -0700

    Added structs for the vertex and model type definitions in the model class header.

--- Test engine/Model.h
+++ Test engine/Model.h
@@ -1,1 +1,52 @@
 #pragma once
+#include <D3D11.h>
+#include <D3DX10math.h>
+#include <fstream>
+using namespace std;
+namespace JR_Model
+{
+	//Model-- a class that contains the vertices and other information for a model
+	class Model
+	{
+	private:
+		//Vertex-- contains the information for a single vertex
+		struct Vertex {
+			//Vertex--The position of the vertex(x,y,z)
+			D3DXVECTOR3 position;
+			//Texture--The tex coords(U,V)
+			D3DXVECTOR2 texture;
+			//Normal--the normal of this vertex
+			D3DXVECTOR3 normal;
+		};
+
+		//ModelType--The model type information
+		struct ModelType
+		{
+			//x-- The x coord
+			float x,
+				//y-- the y coord
+				y,
+				//z-- the z coord
+				z;
+			//tu-- the textures u coord
+			float tu,
+				//tv-- the textures v coord
+				tv;
+			//nx-- the normal x coord
+			float nx,
+				//ny-- the normals y coord
+				ny,
+				//nz-- the normals z coord
+				nz;
+		};
+	public:
+		//defualt constructor--creates a new Model instance with defualt parameters
+		Model();
+		//deconstructor--  cleans up dynamic memory of the model object
+		~Model();
+	private:
+
+	private:
+
+	};
+}
