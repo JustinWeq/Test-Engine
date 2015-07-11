7c66c70b5898e09ad7726993d6e856a2b6894fac
JustinWeq <zoomdude111@hotmail.com>
2015-07-10 17:53:06 -0700

    Added properties to the FBXModel class header in FBXModel.h

--- Test engine/FBXModel.h
+++ Test engine/FBXModel.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <D3D11.h>
+#include <D3DX10math.h>
+
+namespace JR_FBXModel
+{
+	class FBXModel
+	{
+
+
+	private:
+		//vertexBuffer-- the vertex buffer for the model
+		ID3D11Buffer* m_vertexBuffer;
+		//indexBuffer-- the index buffer for the model
+		ID3D11Buffer* m_indexBuffer;
+		//numOfVertices-- the number of vertices
+		unsigned int m_numOfVerticies;
+		//numOfIndicies-- the number of indicies
+		unsigned int m_numOfIndicies;
+
+	};
+}
