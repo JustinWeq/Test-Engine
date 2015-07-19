fe98d67bf2344d9366fc3eca258b89208fc5d91a
JustinWeq <zoomdude111@hotmail.com>
2015-07-19 13:01:11 -0700

    Tweaked the way the cube rotates.

--- Test engine/FBXModel.h
+++ Test engine/FBXModel.h
@@ -6,6 +6,29 @@ namespace JR_FBXModel
 {
 	class FBXModel
 	{
+	public:
+		FBXModel();
+		~FBXModel();
+
+		//init-- sets up the model
+		//device- the device to use for loading
+		//modelName- the name of the model to use
+		bool init(ID3D11Device device,char* modelName);
+
+		//render-- prepares the vertex and index buffers on the device context
+		//deviceContext- the device context to use for rendering
+		void render(ID3D11DeviceContext deviceContext);
+
+	private:
+
+		//loadModel-- loads and sets up the model
+		//device- the deivce to use for loading the model
+		//modelName- the name of the model to load
+		bool loadModel(ID3D11Device device,char* modelName);
+
+		//initBuffers-- initializes the vertex and index buffers for the model
+		//device- the device to use for initialization
+		bool initBuffers(ID3D11Device device);
 
 
 	private:
